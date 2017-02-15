c243a39afc533045e12846896b388da924b24d3f
FoxMessenger <liamfox@MNML.hsd1.il.comcast.net>
2017-02-14 22:03:50 -0600

    game is working - I have buttons, categories, words, and game over.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,12 @@
  
 	// NOTES:
+
+		// needed 	- press any key to get started.
+		//			- theme
+		//			- Wins
+		//			- play again function
+
+		// extra	- play a sound	
 	
 		// --needed-- you will use this code to make sure that the prompted word becomes all lower case so you don't have to have upper and lower case sensitive option -//
 		// String.fromCharCode(event.keyCode).toLowerCase();
@@ -18,11 +25,23 @@ window.onload = function() {
 
  	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']	// game visuals 
 
- 	var word = prompt("Give me a word");			// this will be the word we'll use each round
+ 	var word; //prompt("Give me a word");			// this will be the word we'll use each round
 
  	var guess;										// guess
 	var guesses = [];								// stores the guesses made
     var lives;										// lives
+    var playAgain;
+    var wins;		
+    var space;										// the space between words "-"
+    // var dashReplace = space.replace("-", " ");		// replaces the space character with an actual space
+	// the theme array
+    var theme = [
+    		["gladiator", "john-wick",  "jack-reacher", "enter-the-dragon", "the-raid", "the-bourne-identity", "mad-max", "taken"], //action movies
+    		["the-last-airbender", "vampires-suck", "battlefield-earth", "speed-two-cruise-control", "fear-dot-com", "daddy-day-camp", "jaws-the-revenge", "fifty-shades-of-grey",], //terrible movies
+    		["blair-witch-project", "alien", "prometheus", "cabin-in-the-woods", "scream", "poltergeist"], // horror movies
+    		["fences", "moonlight", "shawshank-redemption", "a-beautiful-mind", "good-will-hunting", "cast-away"], //dramas
+    		["superbad", "airplane", "bridesmaids", "monty-python-and-the-holy-grail"] //comedies
+    		];	
 
     var correctGuesses;								// correct guesses
 	
@@ -58,28 +77,30 @@ window.onload = function() {
 		var correctAnswer = document.createElement("ul");
 		
 		for (var i = 0; i < word.length; i++) {
-					correctAnswer.setAttribute("class", "myWord"); 
-					
-					guess = document.createElement("li");
-					guess.setAttribute("class", "guess");
-					guess.innerHTML = "_";
+			correctAnswer.setAttribute("class", "myWord"); 
+			guess = document.createElement("li");
+			guess.setAttribute("class", "guess");
+			if (word[i] === "-") {
+				guess.innerHTML = "-";
+				space = 1;
+			} else {
+				guess.innerHTML = "_";
+			}
 
-					guesses.push(guess);
-					wordContainer[0].appendChild(correctAnswer);
-					correctAnswer.appendChild(guess);
+			guesses.push(guess);
+			wordContainer[0].appendChild(correctAnswer);
+			correctAnswer.appendChild(guess);
 		}
 	}
 
 	// scoreboard visual
 	var scoreboard = function() {
 		showLives.innerHTML = "You have " + lives + " lives left";
-		console.log('lives are', showLives);
-
 		if (lives < 1) {
 			showLives.innerHTML = "GameOver";
 		}
 		for (var i = 0; i < guesses.length; i++) {
-			if (correctGuesses === guesses.length) {
+			if (correctGuesses + space === guesses.length) {
 				showLives.innnerHTML = "YouWin";
 			}
 		}
@@ -96,6 +117,7 @@ window.onload = function() {
     			if (word[i] === guess) {
     				guesses[i].innerHTML = guess;
     				correctGuesses += 1;
+    				console.log("are have guessed " + correctGuesses + " correctly.");
     			}
     		}
     		// explain this portion
@@ -109,20 +131,51 @@ window.onload = function() {
     	}
     }
 
+	// selecting the theme
+	var themeSelect = function () {
+		// decide whether the theme is in array 0-4
+		if (themeIs === theme[0]) {
+			themeName.innerHTML = "The theme is action movies";
+		} else if (themeIs === theme[1]) {
+			themeName.innerHTML = "The theme is terrible movies";
+		} else if (themeIs === theme[2]) {
+			themeName.innerHTML = "The theme is horror flicks";
+		} else if (themeIs === theme[3]) {
+			themeName.innerHTML = "The theme is drama flicks";
+		} else if (themeIs === theme[4]) {
+			themeName.innerHTML = "The theme is comedy movies";
+		}
+	}
+
 
     var game = function() {
-    	word = word.replace(/\s/g, "-");
-		console.log(word);
-		myButtons();
+
+    	themeIs = theme[Math.floor(Math.random() * theme.length)]; // themeIs is equal to the theme variable array, which is passed through an RNG of the theme length. This gives us 1 of 4 options
+    	word = themeIs[Math.floor(Math.random() * themeIs.length)]; // the word is then replaced with whatever the RNG word from themeIs (1 of 4), but then ran through and actual word -- actually I'm not sure why this works.
+    	// word = word.replace("-", " "); // this doesn't work
+    	console.log(word);
+
 
     	guesses = [];
     	lives = 10;
     	correctGuesses = 0;
+    	space = 0;
 
+		myButtons();
+		themeSelect();
 		wordGuess();
 		scoreboard();
     }
 
     game();
 
+
 }
+
+
+	// Reset Game
+
+	document.getElementById("reset").on("click", function(){
+		correctAnswer.removeChild(correctAnswer);
+		letters.removeChild
+	});
